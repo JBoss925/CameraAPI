3df022e23700be7a3c050a31772008c9b369b5d9
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2014-08-02 15:40:37 +0100

    First commit

---
+++ src/ca/mera/CameraAPI.java
@@ -0,0 +1,15 @@
+package ca.mera;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A camera based plugin which allows the player to neatly navigate their player
+ * in a camera like sense.
+ * 
+ * @author Kieron Wiltshire
+ * @author CurseLuke
+ * 
+ */
+public class CameraAPI extends JavaPlugin {
+
+}
